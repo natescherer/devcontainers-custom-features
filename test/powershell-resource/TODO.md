d3292f63e4e46f6d1c880ad0da30c4da3316c5af
Nate Scherer <376408+natescherer@users.noreply.github.com>
2022-10-18 19:30:05 -0400

    Adjust tests

---
+++ test/powershell-resource/TODO.md
@@ -0,0 +1,5 @@
+# Scenarios that can't currently be implemented
+
+- When pwsh isn't installed
+- When both inputs are used
+- When no inputs are used
--- test/powershell-resource/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-source dev-container-features-test-lib
-
-# There should be a test here for PowerShell not being installed in the container, but, due to limitations in the test suite, it's not currently writeable.
-check "no options" powershell-resource | grep 'Either requiredResource or requiredResourceFile'
-
-reportResults
