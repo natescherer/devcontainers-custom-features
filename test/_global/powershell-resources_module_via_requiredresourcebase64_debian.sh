af210693ba05290e9b88ce0b315497b61034c926
Nate Scherer <376408+natescherer@users.noreply.github.com>
2022-10-19 17:33:29 -0400

    Adjust scope

--- test/_global/powershell-resources_module_via_requiredresourcebase64_debian.sh
+++ test/_global/powershell-resources_module_via_requiredresourcebase64_debian.sh
@@ -3,7 +3,7 @@
 set -e
 source dev-container-features-test-lib
 
-pwsh -c 'Get-PSResource | fl'
-check "ChangelogManagement Latest" pwsh -c 'Get-PSResource -Name ChangelogMangement' | grep 'ChangelogManagement'
+pwsh -c 'Get-PSResource -Scope AllUsers  | fl'
+check "ChangelogManagement Latest" pwsh -c 'Get-PSResource -Scope AllUsers -Name ChangelogMangement' | grep 'ChangelogManagement'
 
 reportResults
--- test/_global/powershell-resources_script_via_requiredresourcebase64_ubuntu.sh
+++ test/_global/powershell-resources_script_via_requiredresourcebase64_ubuntu.sh
@@ -3,7 +3,7 @@
 set -e
 source dev-container-features-test-lib
 
-pwsh -c 'Get-PSResource | fl'
-check "New-OnPremiseHybridWorker Latest" pwsh -c 'Get-PSResource -Name New-OnPremiseHybridWorker' | grep 'New-OnPremiseHybridWorker'
+pwsh -c 'Get-PSResource -Scope AllUsers | fl'
+check "New-OnPremiseHybridWorker Latest" pwsh -c 'Get-PSResource -Scope AllUsers -Name New-OnPremiseHybridWorker' | grep 'New-OnPremiseHybridWorker'
 
 reportResults
