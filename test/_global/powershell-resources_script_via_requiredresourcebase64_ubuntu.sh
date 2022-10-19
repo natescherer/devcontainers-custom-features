64e2f35340ad89001f3cb037522044455c7decd1
Nate Scherer <376408+natescherer@users.noreply.github.com>
2022-10-19 17:22:27 -0400

    Update tests

--- test/_global/powershell-resources_script_via_requiredresourcebase64_ubuntu.sh
+++ test/_global/powershell-resources_script_via_requiredresourcebase64_ubuntu.sh
@@ -3,6 +3,7 @@
 set -e
 source dev-container-features-test-lib
 
+pwsh -c 'Get-PSResource'
 check "New-OnPremiseHybridWorker Latest" pwsh -c 'Get-PSResource -Name New-OnPremiseHybridWorker' | grep 'New-OnPremiseHybridWorker'
 
 reportResults
