1db85f375eb443a4192598054377c23b6fb0f52f
Nate Scherer <376408+natescherer@users.noreply.github.com>
2022-10-19 17:50:52 -0400

    Fix tests

--- test/_global/powershell-resources_module_via_requiredresourcebase64_ubuntu.sh
+++ test/_global/powershell-resources_module_via_requiredresourcebase64_ubuntu.sh
@@ -4,6 +4,6 @@ set -e
 source dev-container-features-test-lib
 
 pwsh -c 'Get-PSResource -Scope AllUsers'
-check "ChangelogManagement Latest" pwsh -c '$Results = Get-PSResource -Scope AllUsers -Name ChangelogMangement; if (!$Results) { throw }'
+check "ChangelogManagement Latest" pwsh -c '$Results = Get-PSResource -Scope AllUsers -Name ChangelogManagement; if (!$Results) { throw }'
 
 reportResults
