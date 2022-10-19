7efb52ddfcea50b093cad96dd6c25d29a9ad9500
Nate Scherer <376408+natescherer@users.noreply.github.com>
2022-10-19 17:55:42 -0400

    Fix spelling

--- test/_global/powershell-resources_module_specific_version_debian.sh
+++ test/_global/powershell-resources_module_specific_version_debian.sh
@@ -4,6 +4,6 @@ set -e
 source dev-container-features-test-lib
 
 pwsh -c 'Get-PSResource -Scope AllUsers'
-check "ChangelogManagement 2.1.3" pwsh -c '$Results = Get-PSResource -Scope AllUsers -Name ChangelogMangement -Version 2.1.3.0; if (!$Results) { throw }'
+check "ChangelogManagement 2.1.3" pwsh -c '$Results = Get-PSResource -Scope AllUsers -Name ChangelogManagement -Version 2.1.3.0; if (!$Results) { throw }'
 
 reportResults
--- test/_global/powershell-resources_module_specific_version_ubuntu.sh
+++ test/_global/powershell-resources_module_specific_version_ubuntu.sh
@@ -4,6 +4,6 @@ set -e
 source dev-container-features-test-lib
 
 pwsh -c 'Get-PSResource -Scope AllUsers'
-check "ChangelogManagement Latest" pwsh -c '$Results = Get-PSResource -Scope AllUsers -Name ChangelogMangement -Version 2.1.3.0; if (!$Results) { throw }'
+check "ChangelogManagement Latest" pwsh -c '$Results = Get-PSResource -Scope AllUsers -Name ChangelogManagement -Version 2.1.3.0; if (!$Results) { throw }'
 
 reportResults
