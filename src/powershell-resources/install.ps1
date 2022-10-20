2b883324fc30d9f928c75983ab6ac9d9857c0cf3
Nate Scherer <376408+natescherer@users.noreply.github.com>
2022-10-20 15:50:59 -0400

    Pin PowerShellGet to 3.0.17-beta17

--- src/powershell-resources/install.ps1
+++ src/powershell-resources/install.ps1
@@ -2,7 +2,7 @@ Write-Host -Object "Ensuring PowerShellGet 3+ is installed..."
 # Ensuring PowerShellGet stable is latest version
 Install-Module -Name PowerShellGet -Force -AllowClobber -Scope AllUsers
 # Installing PowerShellGet 3 Prerelease
-Install-Module -Name PowerShellGet -Force -AllowPrerelease -Scope AllUsers
+Install-Module -Name PowerShellGet -RequiredVersion 3.0.17-beta17 -Force -AllowPrerelease -Scope AllUsers
 
 if ($env:REQUIREDRESOURCEBASE64) {
     $ResourceJson = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($env:REQUIREDRESOURCEBASE64))
