77bfd54df6ce6c451e648569a9f844dc76f80b97
Nate Scherer <376408+natescherer@users.noreply.github.com>
2022-11-10 14:34:22 -0500

    Standardize variables

--- src/k9s/install.sh
+++ src/k9s/install.sh
@@ -11,18 +11,18 @@ find_github_release_asset_url() {
     local version=$3
     local versionPrefix=${4:-"v"}
 
-    if [ "$version" = "latest" ]; then
+    if [ "${version}" = "latest" ]; then
         local releaseUrl="https://api.github.com/repos/${repo}/releases/latest"
     else
         local releaseUrl="https://api.github.com/repos/${repo}/releases/tags/${versionPrefix}${version}"
     fi
-    archRegex="$(dpkg --print-architecture)"
-    if [ "$archRegex" = "amd64" ]; then
-        archRegex="(amd64|x64|x86_64)"
+    arch="$(dpkg --print-architecture)"
+    if [ "${arch}" = "amd64" ]; then
+        arch="(amd64|x64|x86_64)"
     fi
 
-    local filenameRegex=$(echo "$filenameTemplate" | sed "s/VERSION/$version/" | sed "s/ARCH/$arch/")
-    local downloadUrl=$(curl -H "Accept: application/vnd.github+json" -s ${releaseUrl} | jq -r -c ".assets[] | select(.name|test(\"$filenameRegex\")) | .browser_download_url")
+    local filenameRegex=$(echo "$filenameTemplate" | sed "s/VERSION/${version}/" | sed "s/ARCH/${arch}/")
+    local downloadUrl=$(curl -H "Accept: application/vnd.github+json" -s ${releaseUrl} | jq -r -c ".assets[] | select(.name|test(\"${filenameRegex}\")) | .browser_download_url")
 
     echo $downloadUrl
 }
@@ -32,20 +32,19 @@ download_and_install_gzipped_tarball() {
     local destFolder=$2
     local binaryName=$3
 
-    local installTempPath=/tmp/$binaryName
+    local installTempPath="/tmp/${binaryName}"
     local tarballName=$(echo "$url" | rev | cut -d'/' -f1 | rev)
 
     echo "Installing $url to $destFolder..."
 
-    mkdir -p "$installTempPath" "$destFolder"
-    curl -sSL -o "$installTempPath/$tarballName" "$url"
-    tar xf "$installTempPath/$tarballName" -C "$destFolder"
-    ln -s "$destFolder/$binaryName" "/usr/local/bin/$binaryName"
+    mkdir -p "$installTempPath" "/opt/${binaryName}"
+    curl -sSL -o "${installTempPath}/${tarballName}" "$url"
+    tar xf "${installTempPath}/${tarballName}" -C "$destFolder"
+    ln -s "${destFolder}/${binaryName}" "/usr/local/bin/${binaryName}"
     rm -rf "$installTempPath"
 }
 
-ghUrl="$(find_github_release_asset_url $repo $filenameTemplate $VERSION)"
+ghUrl=find_github_release_asset_url $repo $filenameTemplate $VERSION
 
 echo "$ghUrl"
-
-download_and_install_gzipped_tarball $ghUrl /opt/$binaryName $binaryName
+download_and_install_gzipped_tarball "$ghUrl" "/opt/${binaryName}" $binaryName
