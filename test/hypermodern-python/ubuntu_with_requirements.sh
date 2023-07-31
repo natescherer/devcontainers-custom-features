18b0f7546c357cb9b4718893cc353d49bf7c5344
Nate Scherer <376408+natescherer@users.noreply.github.com>
2023-07-31 18:32:51 -0400

    Fix requirementsFile and improve testing

--- test/hypermodern-python/ubuntu_with_requirements.sh
+++ test/hypermodern-python/ubuntu_with_requirements.sh
@@ -3,7 +3,6 @@
 set -e
 source dev-container-features-test-lib
 
-check "python 3.10" asdf list python 3.10
 check "python 3.11" asdf list python 3.11
 
 check "cookiecutter" cookiecutter --version
@@ -11,5 +10,7 @@ check "poetry" poetry --version
 check "nox" nox --version
 pipx list --include-injected --json > /tmp/pipxlist.json
 check "nox-poetry"  yq '.venvs.nox.metadata.injected_packages | has("nox-poetry")' /tmp/pipxlist.json  -o json -e
+
+check "requests installed" pip list | grep -F requests
 
 reportResults
