37988e9e7adb5535aa287af5baae5e3a357040f0
Nate Scherer <376408+natescherer@users.noreply.github.com>
2024-01-23 21:21:33 -0500

    Update scenarios

--- test/powershell-resources/powershell-resources_script_via_requiredresourcejsonfile_debian.sh
+++ test/powershell-resources/powershell-resources_script_via_requiredresourcejsonfile_debian.sh
@@ -3,7 +3,6 @@
 set -e
 source dev-container-features-test-lib
 
-pwsh -c 'Get-PSResource -Scope AllUsers'
-check "Install-GithubRelease Latest" pwsh -c '$Results = Get-PSResource -Scope AllUsers -Name Install-GithubRelease; if (!$Results) { throw }'
+check "Assert-SameFile Latest" pwsh -c '$Results = Get-InstalledPSResource -Scope AllUsers -Name Assert-SameFile; if (!$Results) { throw }'
 
 reportResults
